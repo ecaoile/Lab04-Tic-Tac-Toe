8df39abd257267ec804d60f34b91bd0231fcfe7f
ecaoile <unfie555@gmail.com>
2018-06-10 14:59:01 -0700

    scaffolding of outside while loop

---
+++ Lab04_Tic_Tac_Toe/Lab04_Tic_Tac_Toe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab04_Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runProgram = true;
+            while (runProgram == true)
+            {
+                Console.WriteLine("run game stuff here");
+                Console.WriteLine("Game over! Play again?");
+                string playAgain = Console.ReadLine().ToLower();
+                while (playAgain != "y" && playAgain != "yes" && playAgain != "n" && playAgain != "no")
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                    playAgain = Console.ReadLine().ToLower();
+                }
+
+                if (playAgain == "y" || playAgain == "yes")
+                    Console.Clear();
+                else
+                    runProgram = false;
+            }
+
+            Console.WriteLine("Thank you for playing! Press any button to exit.");
+            Console.ReadLine();
+        }
+    }
+}
