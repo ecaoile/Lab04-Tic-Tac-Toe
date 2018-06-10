83c77405d64fffcfff488290342b8cad1f16ba6e
ecaoile <unfie555@gmail.com>
2018-06-10 15:23:42 -0700

    player object creation working with name writing to console

---
+++ Lab04_Tic_Tac_Toe/Lab04_Tic_Tac_Toe/Classes/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04_Tic_Tac_Toe.Classes
+{
+    class Player
+    {
+        public string Name { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
--- Lab04_Tic_Tac_Toe/Lab04_Tic_Tac_Toe/Program.cs
+++ Lab04_Tic_Tac_Toe/Lab04_Tic_Tac_Toe/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab04_Tic_Tac_Toe.Classes;
+using System;
 
 namespace Lab04_Tic_Tac_Toe
 {
@@ -7,9 +8,38 @@ namespace Lab04_Tic_Tac_Toe
         static void Main(string[] args)
         {
             bool runProgram = true;
+
+            Console.WriteLine("Welcome to Lab 04: Tic-Tac-Toe!\n");
             while (runProgram == true)
             {
-                Console.WriteLine("run game stuff here");
+                bool player1Win = false;
+                bool player2Win = false;
+
+                Console.WriteLine("Player 1, please enter your name:");
+                string player1Name = Console.ReadLine();
+                while (player1Name == "")
+                {
+                    Console.WriteLine("Please enter SOMETHING for a name.");
+                    player1Name = Console.ReadLine();
+                }
+                Player player1 = new Player(player1Name);
+
+                Console.WriteLine("Player 2, please enter your name:");
+                string player2Name = Console.ReadLine();
+                while (player2Name == "")
+                {
+                    Console.WriteLine("Please enter SOMETHING for a name.");
+                    player2Name = Console.ReadLine();
+                }
+                Player player2 = new Player(player2Name);
+
+                Console.WriteLine($"Welcome, {player1.Name} and {player2.Name}.");
+
+                if (player1Win == false && player2Win == false)
+                {
+                    Console.WriteLine("It's a draw!");
+                }
+
                 Console.WriteLine("Game over! Play again?");
                 string playAgain = Console.ReadLine().ToLower();
                 while (playAgain != "y" && playAgain != "yes" && playAgain != "n" && playAgain != "no")
@@ -19,7 +49,10 @@ namespace Lab04_Tic_Tac_Toe
                 }
 
                 if (playAgain == "y" || playAgain == "yes")
+                {
                     Console.Clear();
+                    Console.WriteLine("Let's do this again!");
+                }
                 else
                     runProgram = false;
             }
@@ -27,5 +60,6 @@ namespace Lab04_Tic_Tac_Toe
             Console.WriteLine("Thank you for playing! Press any button to exit.");
             Console.ReadLine();
         }
+
     }
 }
