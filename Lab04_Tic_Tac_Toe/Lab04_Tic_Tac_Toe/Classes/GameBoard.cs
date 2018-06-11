8f5faa9d5dbc3bfd5a93b59c77e9f47cb7144222
ecaoile <unfie555@gmail.com>
2018-06-11 00:43:48 -0700

    full game functionality and comments for methods

--- Lab04_Tic_Tac_Toe/Lab04_Tic_Tac_Toe/Classes/GameBoard.cs
+++ Lab04_Tic_Tac_Toe/Lab04_Tic_Tac_Toe/Classes/GameBoard.cs
@@ -4,44 +4,95 @@ using System.Text;
 
 namespace Lab04_Tic_Tac_Toe.Classes
 {
+    /// <summary>
+    /// class that creates a game board and handles game board changes
+    /// </summary>
     class GameBoard
     {
         public string[][] Layout { get; set; }
+        public string[][] Taken { get; set; }
         public string[][] defaultLayout = new string[][]
             {
                     new string[] { "1", "2", "3" },
                     new string[] { "4", "5", "6" },
                     new string[] { "7", "8", "9" }
             };
+
+        public string[][] defaultTaken = new string[][]
+        {
+            new string[3] ,
+            new string[3] ,
+            new string[3]
+        };
+
         public GameBoard()
         {
             Layout = defaultLayout;
+            Taken = defaultTaken;
         }
 
-        public void DisplayBoard(string[][] datBoard)
+        /// <summary>
+        ///  
+        /// </summary>
+        /// <param name="datBoard"></param>
+        /// <returns></returns>
+        public bool DisplayBoard(string[][] datBoard)
         {
-            Console.WriteLine("Current game board:");
-            for (int i = 0; i < 3; i++)
+            if (datBoard.Length != 3)
+                return false;
+            try
             {
-                for (int j = 0; j < 3; j++)
+                Console.WriteLine("Current game board:");
+                for (int i = 0; i < 3; i++)
                 {
-                    Console.Write($"|{datBoard[i][j]}|");
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.Write($"|{datBoard[i][j]}|");
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error in making the board: {e.Message}");
+                return false;
             }
         }
 
+        /// <summary>
+        /// method for updating the board with the last user's input
+        /// </summary>
+        /// <param name="num">the number which designates the space the user chose</param>
+        /// <param name="datBoard">the current board board in play</param>
+        /// <param name="marker">marker of the current player</param>
+        /// <returns>the updated board containing the last marker</returns>
         public string[][] UpdateBoard(string num, string[][] datBoard, string marker)
         {
-            for (int i = 0; i < 3; i++)
+            try
             {
-                for (int j = 0; j < 3; j++)
+                if (datBoard.Length != 3)
                 {
-                    if (num == datBoard[i][j])
-                        datBoard[i][j] = marker;
+                    return defaultLayout;
                 }
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (num == datBoard[i][j])
+                        {
+                            Taken[i][j] = datBoard[i][j];
+                            datBoard[i][j] = marker;
+                        }
+                    }
+                }
+                return datBoard;
             }
-            return datBoard;
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error trying to update the board: {e.Message}.");
+                return defaultLayout;
+            }
         }
     }
 }
--- Lab04_Tic_Tac_Toe/Lab04_Tic_Tac_Toe/Program.cs
+++ Lab04_Tic_Tac_Toe/Lab04_Tic_Tac_Toe/Program.cs
@@ -5,7 +5,21 @@ namespace Lab04_Tic_Tac_Toe
 {
     class Program
     {
+        /// <summary>
+        /// main entry point to the lab: opens up the game console and starts a tic-tac-toe game
+        /// </summary>
+        /// <param name="args">some stuff needed to run a main method...</param>
         static void Main(string[] args)
+        {
+            PlayGame();
+            Console.WriteLine("Thank you for playing! Press any button to exit.");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// method that runs the game
+        /// </summary>
+        public static void PlayGame()
         {
             bool runProgram = true;
 
@@ -14,7 +28,7 @@ namespace Lab04_Tic_Tac_Toe
             {
                 bool player1Win = false;
                 bool player2Win = false;
-
+                int roundNum = 1;
                 Console.WriteLine("Player 1, please enter your name:");
                 string player1Name = Console.ReadLine();
                 while (player1Name == "")
@@ -37,31 +51,73 @@ namespace Lab04_Tic_Tac_Toe
                 Console.WriteLine($"Welcome, {player1.Name} and {player2.Name}.");
                 Console.WriteLine($"{player1.Name}'s marker: {player1.Marker}.");
                 Console.WriteLine($"{player2.Name}'s marker: {player2.Marker}.");
-                Console.WriteLine($"Ready...FIGHT!\n");
+                Console.WriteLine($"Rouund {roundNum}...FIGHT!");
 
                 GameBoard datGameBoard = new GameBoard();
-                bool player1Turn = true;
-                bool player2True = false;
                 bool takingTurns = true;
-                while (takingTurns = true)
+                int turn = 1;
+                while (takingTurns == true)
                 {
+                    Console.WriteLine();
                     datGameBoard.DisplayBoard(datGameBoard.Layout);
+                    if (turn % 2 != 0)
+                        Console.WriteLine($"{player1.Name}'s turn ({player1.Marker}).");
+                    else
+                        Console.WriteLine($"{player2.Name}'s turn ({player2.Marker}).");
+
                     Console.WriteLine("Choose a number.");
                     string chosenNum = Console.ReadLine();
-                    if (player1Turn == true)
-                        datGameBoard.Layout = datGameBoard.UpdateBoard(chosenNum, datGameBoard.Layout, player1.Marker);
-                    else
-                        datGameBoard.Layout = datGameBoard.UpdateBoard(chosenNum, datGameBoard.Layout, player2.Marker);
-                }
-                datGameBoard.DisplayBoard(datGameBoard.Layout);
+                    bool choseUnique = false;
 
-                Console.ReadLine();
+                    while (choseUnique == false)
+                    {
+                        bool foundMatch = false;
+                        for (int i = 0; i < 3; i++)
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                if (datGameBoard.Taken[i][j] == chosenNum)
+                                    foundMatch = true;
+                            }
+                        }
+                        if (foundMatch == false)
+                            choseUnique = true;
+                        else
+                        {
+                            Console.WriteLine("That position is already taken!");
+                            chosenNum = Console.ReadLine();
+                        }
+                    }
+
+
+                    if (turn % 2 != 0)
+                    {
+                        datGameBoard.Layout = datGameBoard.UpdateBoard(chosenNum, datGameBoard.Layout, player1.Marker);
+                        player1Win = CheckForWin(datGameBoard.Layout, player1.Marker);
+                    }
+                    if (turn % 2 == 0)
+                    {
+                        datGameBoard.Layout = datGameBoard.UpdateBoard(chosenNum, datGameBoard.Layout, player2.Marker);
+                        player2Win = CheckForWin(datGameBoard.Layout, player2.Marker);
+                    }
+                    if (player1Win == true || player2Win == true)
+                        takingTurns = false;
+                    if (turn == 9)
+                        takingTurns = false;
+                    turn++;
+                }
+
+                if (player1Win == true)
+                    Console.WriteLine("Player 1 wins!");
+                if (player2Win == true)
+                    Console.WriteLine("Player 2 wins!");
+
                 if (player1Win == false && player2Win == false)
                 {
                     Console.WriteLine("It's a draw!");
                 }
 
-                Console.WriteLine("Game over! Play again?");
+                Console.WriteLine("\nGame over! Play again?");
                 string playAgain = Console.ReadLine().ToLower();
                 while (playAgain != "y" && playAgain != "yes" && playAgain != "n" && playAgain != "no")
                 {
@@ -73,14 +129,64 @@ namespace Lab04_Tic_Tac_Toe
                 {
                     Console.Clear();
                     Console.WriteLine("Let's do this again!");
+                    roundNum++;
                 }
                 else
                     runProgram = false;
             }
-
-            Console.WriteLine("Thank you for playing! Press any button to exit.");
-            Console.ReadLine();
         }
 
+        /// <summary>
+        /// method that checks whether a player won
+        /// </summary>
+        /// <param name="datBoard">the current board with the chosen spaces marked</param>
+        /// <param name="marker">the marker being checked for a win</param>
+        /// <returns>true if the player won and false if they didn't or an error occurred</returns>
+        public static bool CheckForWin(string[][] datBoard, string marker)
+        {
+            // allow only X and O for markers
+            if (marker != "X" && marker != "O")
+                return false;
+            try
+            {
+                string topLeft = datBoard[0][0];
+                string topCenter = datBoard[0][1];
+                string topRight = datBoard[0][2];
+                string centerLeft = datBoard[1][0];
+                string datCenter = datBoard[1][1];
+                string centerRight = datBoard[1][2];
+                string bottomLeft = datBoard[2][0];
+                string bottomCenter = datBoard[2][1];
+                string bottomRight = datBoard[2][2];
+
+                // check for horizontal values
+                if (topLeft == marker && topCenter == marker && topRight == marker)
+                    return true;
+                if (centerLeft == marker && datCenter == marker && centerRight == marker)
+                    return true;
+                if (bottomLeft == marker && bottomCenter == marker && bottomRight == marker)
+                    return true;
+
+                // check for vertical values
+                if (topLeft == marker && centerLeft == marker && bottomLeft == marker)
+                    return true;
+                if (topCenter == marker && datCenter == marker && bottomCenter == marker)
+                    return true;
+                if (topRight == marker && centerRight == marker && bottomRight == marker)
+                    return true;
+
+                // check for diagonal values
+                if (topLeft == marker && datCenter == marker && bottomRight == marker)
+                    return true;
+                if (topRight == marker && datCenter == marker && bottomLeft == marker)
+                    return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error trying to check for win: {e.Message}.");
+                return false;
+            }
+            return false;
+        }
     }
 }
